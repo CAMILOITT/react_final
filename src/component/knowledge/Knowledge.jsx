dbac6c4caea6a980f1ed0d55d644b30de06554c0
CAMILOITT <camilocamilo5@outlook.com>
2022-09-11 22:51:17 -0500

    add logo

--- src/component/knowledge/Knowledge.jsx
+++ src/component/knowledge/Knowledge.jsx
@@ -1,30 +1,55 @@
 import css from './Knowledge.module.css';
 
+import htmlLogo from '../../assets/logoLanguage/html.svg';
+import cssLogo from '../../assets/logoLanguage/css.svg';
+// import sassLogo from '../../assets/logoLanguage/sass.svg';
+import bootstrapLogo from '../../assets/logoLanguage/bootstrap.svg';
+import tailwindLogo from '../../assets/logoLanguage/tailwind.svg';
+import javascriptLogo from '../../assets/logoLanguage/javascript.svg';
+import reactLogo from '../../assets/logoLanguage/react.svg';
+import uiComponentsLogo from '../../assets/logoLanguage/ui-components.svg';
+
 export default function knowledge() {
   return (
     <div className={css.containerKnowledge}>
       <h3 className={css.title}>Mis conocimientos</h3>
       <div className={css.containerLenguajes}>
-        <img className={`${css.iconHtml} ${css.icon}`} src="" alt="html" />
-        <img className={`${css.iconCss} ${css.icon}`} src="" alt="css" />
-        <img className={`${css.iconSass} ${css.icon}`} src="" alt="SASS" />
+        <img
+          className={`${css.iconHtml} ${css.icon}`}
+          src={htmlLogo}
+          alt="html"
+        />
+        <img className={`${css.iconCss} ${css.icon}`} src={cssLogo} alt="css" />
+        <img
+          className={`${css.iconSass} ${css.icon}`}
+          // src={sassLogo}
+          alt="SASS"
+        />
         <img
           className={`${css.iconBootstrap} ${css.icon}`}
-          src=""
+          src={bootstrapLogo}
           alt="bootstrap"
         />
         <img
           className={`${css.iconTailwind} ${css.icon}`}
-          src=""
+          src={tailwindLogo}
           alt="tailwind"
         />
-        <img className={`${css.iconJS} ${css.icon}`} src="" alt="js" />
+        <img
+          className={`${css.iconJS} ${css.icon}`}
+          src={javascriptLogo}
+          alt="javascript"
+        />
         <img
           className={`${css.iconUiComponents} ${css.icon}`}
-          src=""
+          src={uiComponentsLogo}
           alt="ui-components"
         />
-        <img className={`${css.iconReact} ${css.icon}`} src="" alt="React" />
+        <img
+          className={`${css.iconReact} ${css.icon}`}
+          src={reactLogo}
+          alt="React"
+        />
       </div>
     </div>
   );
